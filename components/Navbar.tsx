4cde55dd43d2e64e883451077b1a138aa97ecc4e
Maulana Faqih <maulanafaqih54@gmail.com>
2026-06-05 05:03:59 +0700

    feat: redesign navbar with split layout and center brand

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,12 +2,24 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-inner">
-        <div className="nav-logo">maulanafaqih<span>.dev</span></div>
-        <div className="nav-links">
-          <a href="#about">about</a>
-          <a href="#experience">experience</a>
-          <a href="#projects">projects</a>
-          <a href="#contact">contact</a>
+        <div className="nav-links nav-links-left">
+          <a href="#about">About</a>
+          <a href="#experience">Experience</a>
+        </div>
+
+        <div className="nav-center">
+          <div className="nav-center-icon">
+            <span>MF</span>
+          </div>
+          <div className="nav-center-text">
+            <div className="nav-center-name">MAULANA FAQIH</div>
+            <div className="nav-center-sub">Computer Engineer &amp; IT Specialist</div>
+          </div>
+        </div>
+
+        <div className="nav-links nav-links-right">
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
         </div>
       </div>
     </nav>
